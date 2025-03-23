38c55610d34110c49415ff354ee8eec4a37b81aa
Bắc <you@example.com>
2025-03-23 12:45:31 +0700

    add router in project

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,36 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import ErrorPage from './pages/error.page.tsx';
+import HomePage from './pages/home.page.tsx';
+
+
+const router = createBrowserRouter([
+
+   // Manager Client 
+   {
+    path: "/",
+    element: <App/>,
+    errorElement: <ErrorPage />,
+    children:[
+      { index: true,  element: <HomePage/>,},
+      // {
+      //   path: "contacts/:contactId",
+      //   element: <HomePage />,
+      // },
+
+    ],
+  }
+
+
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+   <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/pages/home.page.tsx
@@ -0,0 +1,17 @@
+
+
+const HomePage =()=> {
+
+
+    return (
+
+    <>
+    <div>
+        <h1>Home Page </h1>
+    </div>
+    </>
+    )
+
+}
+
+export default HomePage 
